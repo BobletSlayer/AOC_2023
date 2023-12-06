59706bce6064c92578a2ad60f8cb1a325f959d4e
Boblet <robi.cserpak@gmail.com>
2023-12-06 22:25:31 +0100

    Day 3 Part 2

--- src/main/scala/Day3.scala
+++ src/main/scala/Day3.scala
@@ -1,15 +1,20 @@
 import scala.util.boundary, boundary.break
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
 class Day3 {
   def print: Unit = {
     println("First Part:")
     println(firstPart)
     println("Second Part:")
     println(secondPart)
-    println(lines.mkString("\n"))
-    println(generatePositions(0,0).filter(positionInArray))
+    // println(findNum(0, 0))
+    // println(lines(0)(0))
+    // println(lines(0)(0).isDigit)
+    // println(lines.mkString("\n"))
+    // println(generateAdjacentPositions(0, 0).filter(positionInArray))
   }
 
-  private val source = scala.io.Source.fromFile("Input\\3\\test1.txt")
+  private val source = scala.io.Source.fromFile("Input\\3\\input.txt")
   val stringSrc =
     try source.mkString
     finally source.close()
@@ -20,33 +25,70 @@ class Day3 {
   val width = lines(0).length
   val height = lines.length
 
-  val firstPart = "TODO"
-  val secondPart = "TODO"
+  val firstPart =
+    lines.zipWithIndex.map(x => lineToNumbers(x(0), x(1))).flatten.sum
+  val secondPart =
+    lines.zipWithIndex.map(findGears).flatten.map(processGear).sum
+  // val asd =
+  //   lines.drop(1).zipWithIndex.map(findGears).flatten.map(processGear).sum
 
-  // def getNumbers(line: Array[String], row:Int, col:Int = 0) = line match{
-  //   case x :: xs if Character.isDigit(x) => {
-  //     var num = (x ++ xs.takeWhile(Character.isDigit)).toInt
-  //     // checkNumber(num,) 
-  //   }
-  //   }
-  
+  def lineToNumbers(
+      line: String,
+      row: Int
+  ): List[Int] =
+    var i = 0
+    var positions = ListBuffer[Int]()
+    while i < line.length do
+      var adjacentSymbol = false
+      var sNum = ""
+      while i < line.length && Character.isDigit(line(i)) do
+        adjacentSymbol = adjacentSymbol || checkPosition(row, i)
+        sNum += (line(i))
+        i += 1
+      if !sNum.isEmpty() then
+        val num = sNum.toInt
+        if adjacentSymbol then positions += num
+      i += 1
+    positions.toList
 
+  def checkPosition(position: (Int, Int)): Boolean =
+    generateAdjacentPositions(position)
+      .filter(positionInArray)
+      .exists(isSymbol)
 
-  def checkNumber(num:Int, positions:Array[(Int,Int)]) =
-    (num, checkPositions(positions))
-    
-  def checkPositions (positions:Array[(Int,Int)]) =
-    positions.map(generatePositions).flatten.filter(positionInArray).exists(isSymbol)
+  def generateAdjacentPositions(pos: (Int, Int)): Seq[(Int, Int)] =
+    for
+      i <- -1 to 1
+      j <- -1 to 1
+    yield (pos(0) + i, pos(1) + j)
 
-  def generatePositions (pos:(Int,Int)) = 
-    for i <- -1 to 1
-        j <- -1 to 1
-    yield (pos(0)+i, pos(1)+j) 
-
-  def isSymbol (x:Int, y:Int) = 
+  def isSymbol(x: Int, y: Int): Boolean =
     lines(x)(y) != '.' && !Character.isDigit(lines(x)(y))
 
-  def positionInArray(x:(Int, Int)) = x match
-    case (x,y) => !(x < 0 || x >= height || y < 0 || y >= width)
+  def positionInArray(x: (Int, Int)): Boolean = x match
+    case (x, y) => !(x < 0 || x >= height || y < 0 || y >= width)
 
+  def processGear(pos: (Int, Int)): Int =
+    // does not work if adjacent numbers are the same :(
+    val nums = generateAdjacentPositions(pos).map(findNum).flatten.distinct
+    if nums.length > 1 then nums.product else 0
+
+  def findNum(pos: (Int, Int)): Option[Int] = pos match
+    case (row, col) =>
+      var x = row
+      var y = col
+      if lines(x)(y).isDigit
+      then
+        while y > 0 && lines(x)(y - 1).isDigit
+        do y -= 1
+        Some(lines(x).drop(y).takeWhile(_.isDigit).toInt)
+      else None
+
+  def findGears(line: (String, Int)): Seq[(Int, Int)] = line match
+    case (str, row) =>
+      str.zipWithIndex.filter((x, y) => x == '*').map((x, y) => (row, y))
+  // lazy val findGears: ((String, Int)) => Seq[(Int, Int)] = 
+  //     (str, row) =>
+  //       str.zipWithIndex.filter((x, y) => x == '*').map((x, y) => (row, y))
+  
 }
